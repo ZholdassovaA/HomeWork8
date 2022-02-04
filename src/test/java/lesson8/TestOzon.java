fc1fafa6e7915f7be0979833bf7dbe81321189c8
Aidana <zholdassova.aidana@mail.ru>
2022-02-04 20:23:25 +0600

    added MethodSource

--- src/test/java/lesson8/TestOzon.java
+++ src/test/java/lesson8/TestOzon.java
@@ -1,11 +1,16 @@
 package lesson8;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Selenide.*;
@@ -21,6 +26,24 @@ public class TestOzon {
         $$(".widget-search-result-container.a9x2").shouldHave(texts("Adidas"));
     }
 
+    static Stream<Arguments> testWithMethodSource() {
+        return Stream.of(
+                Arguments.of("Adidas", "Футбольный мяч adidas"),
+                Arguments.of("Chanel", "Chanel Coco Mademoiselle")
+        );
+    }
+
+    @MethodSource
+    @Tag("blocker")
+    @DisplayName("Поиск бренда Adidas на сайте Озон")
+    @ParameterizedTest(name = "Поиск на сайте Озон {0} и проверка отображения товаров {1}")
+    void testWithMethodSource (String SearchBrand, String expectedResult) {
+        open("https://www.ozon.ru/");
+        $(".f9j4").setValue(SearchBrand);
+        $(".f9j4 [type = 'text']").click();
+        $$("widget-search-result-container.a9x2").find(Condition.text(expectedResult)).shouldBe(Condition.visible);
+    }
+
     //@ValueSource (strings = {"Adidas", "Chanel"})
     @CsvSource({
             "Adidas,Футбольный мяч adidas\"Adidas\"",
@@ -28,10 +51,10 @@ public class TestOzon {
     })
     @Tag("blocker")
     @ParameterizedTest(name = "Поиск бренда {1} на сайте Озон")
-    void searchChanelTest(String searchQuery, String expectedResult) {
+    void searchChanelTest(String SearchBrand, String expectedResult) {
         open("https://www.ozon.ru/");
         $(".f9j4").click();
-        $(".f9j4 [type = 'text']").setValue(searchQuery).pressEnter();
+        $(".f9j4 [type = 'text']").setValue(SearchBrand).pressEnter();
         $$(".widget-search-result-container.a9x2").shouldHave(texts(expectedResult));
     }
 
